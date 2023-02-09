e8045304b5cebd920e4f02db1898f32640a7ee84
Juan Antonio Marquez Ruiz <bymarquezz2@gmail.com>
2023-02-09 22:52:35 +0100

    Update index.js

--- src/cuerpo/index.js
+++ src/cuerpo/index.js
@@ -2,12 +2,27 @@ import {React, useEffect, useState} from 'react';
 
 function Cuerpo() {
   const [listaPokemon, setListaPokemons] = useState([]);
+  const [listaCompleta, setlistaCompleta] = useState([]);
   const [urlPokeApi, setUrlPokeApi] = useState('https://pokeapi.co/api/v2/pokemon?limit=8');
   useEffect(() => {fetchPokemon();}, [])
   function fetchPokemon() {
     fetch('https://pokeapi.co/api/v2/pokemon?limit=8')
     .then(response => response.json())
-    .then(data => {console.log(data);setListaPokemons(data.results)});
+    .then(data => {
+      console.log(data);
+      setListaPokemons(data.results);
+      cargarAllInfo();
+    });
+  }
+  function cargarAllInfo() {
+    listaPokemon.map( (pokemon, index) => {
+      fetch(pokemon.url)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        setlistaCompleta(listaCompleta.push(data));
+      });
+    })
   }
   function cargaTodos() {
     fetch(urlPokeApi)
@@ -25,8 +40,8 @@ function Cuerpo() {
     <>
       <ul>
         {
-          listaPokemon.map( (pokemon, index) => 
-            <li key={index}>{pokemon.name}</li>
+          listaCompleta.map( (pokemon) => 
+            <li>{pokemon.name}</li>
           )
         }
       </ul>
